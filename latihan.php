5040e22a3dd2b179727bf06834cacb5e97f8b851
farhanmuzam <145105831+farhanmuzam@users.noreply.github.com>
2023-10-19 21:29:39 +0700

    latihan.php

--- latihan.php
+++ latihan.php
@@ -1,32 +1,33 @@
 <?php
-$title = "Welcome Student Of Telkom University!";
+$title = "Welcome Student Of TelU";
 ?>
 
 <html>
     <head>
-        <h1><?php echo $title; ?></h1> 
+        <title><?php echo $title; ?></title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
-    <br>
     <body>
+        <h1><?php echo $title; ?></h1>
         <form action="latihan3.php" method="get">
-         <label for="nomor">Insert Number!</label>
-        <input type="number" name="nomor" required>
-         <br>
-         <br>
-        <label for="choose">Choose Number!</label>
-        <select name="choose">
-        <option value="choose1">1</option>
-        <option value="choose2">2</option>
-        <option value="choose2">3</option>
-        <option value="choose2">4</option>
-        <option value="choose2">5</option>
-        <option value="choose2">6</option>
-        </select>
-         <br>
-         <br>
-         <button type="submit" name="count">Submit untuk hitung!</button>
+            <div class="form-group">
+                <label for="angkaInput">Insert Number!:</label>
+                <input type="number" class="form-control" id="angkaInput" name="nomor" placeholder="Input Only Number">
+            </div>
+            <div class="form-group">
+                <label for="dropdownInput">Choose Number!:</label>
+                <select class="form-control" id="dropdownInput" name="pilihan">
+                    <option value="pilihan1">1</option>
+                    <option value="pilihan2">2</option>
+                    <option value="pilihan3">3</option>
+                    <option value="pilihan3">4</option>
+                    <option value="pilihan3">5</option>
+                    <option value="pilihan3">6</option>
+                </select>
+            </div>
+            <br>
+            <br>
+            <button type="submit">Submit For Count!</button>
         </form>
     </body>
-    
 </html>
